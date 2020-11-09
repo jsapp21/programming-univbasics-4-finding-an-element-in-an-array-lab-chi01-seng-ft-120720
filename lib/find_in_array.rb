6d205b72df20705cc663f3e445de41fc3e9b474b
James Sapp <jameseisapp@gmail.com>
2020-11-09 17:22:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   new_array = [] 
   
   array.length.times do |index|
-    new_array.push(array[index])
+    new_array.push(array.include?[index])
   end
   new_array
 
